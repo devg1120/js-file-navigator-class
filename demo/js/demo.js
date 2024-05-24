b326b6c773e12536f3f7f6e897ec84ae313f8e2b
unknown <devg1120@gmail.com>
2024-05-24 15:21:30 +0900

    up

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,15 +1,9 @@
 //import './routes.js';
 import "../../src/file-tree.js";
 
-//import { FileTreeControl } from './routes2.js';
+
 import { FileTreeControl } from "./fileTreeControl.js";
 
 let fc = new FileTreeControl();
 
-/*
-function loadFinished(){
-  let fc = new FileTreeControl();
-}
 
-window.addEventListener('load', loadFinished);
-*/
